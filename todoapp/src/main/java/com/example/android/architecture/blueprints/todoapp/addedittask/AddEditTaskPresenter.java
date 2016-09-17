1bd807e783c3c4e3fb5f3264436588f34b00c093
LiYiHeng <liyiheng@superloop.com.cn>
2016-09-17 18:53:10 +0800

    TODO-MVP added as a demo.

---
+++ todoapp/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/AddEditTaskPresenter.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.architecture.blueprints.todoapp.addedittask;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.example.android.architecture.blueprints.todoapp.data.Task;
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource;
+import com.example.android.architecture.blueprints.todoapp.util.schedulers.BaseSchedulerProvider;
+
+import rx.Observer;
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Listens to user actions from the UI ({@link AddEditTaskFragment}), retrieves the data and updates
+ * the UI as required.
+ */
+public class AddEditTaskPresenter implements AddEditTaskContract.Presenter {
+
+    @NonNull
+    private final TasksDataSource mTasksRepository;
+
+    @NonNull
+    private final AddEditTaskContract.View mAddTaskView;
+
+    @NonNull
+    private final BaseSchedulerProvider mSchedulerProvider;
+
+    @Nullable
+    private String mTaskId;
+
+    @NonNull
+    private CompositeSubscription mSubscriptions;
+
+    /**
+     * Creates a presenter for the add/edit view.
+     *
+     * @param taskId          ID of the task to edit or null for a new task
+     * @param tasksRepository a repository of data for tasks
+     * @param addTaskView     the add/edit view
+     */
+    public AddEditTaskPresenter(@Nullable String taskId,
+                                @NonNull TasksDataSource tasksRepository,
+                                @NonNull AddEditTaskContract.View addTaskView,
+                                @NonNull BaseSchedulerProvider schedulerProvider) {
+        mTaskId = taskId;
+        mTasksRepository = checkNotNull(tasksRepository, "tasksRepository cannot be null!");
+        mAddTaskView = checkNotNull(addTaskView, "addTaskView cannot be null!");
+        mSchedulerProvider = checkNotNull(schedulerProvider, "schedulerProvider cannot be null!");
+
+        mSubscriptions = new CompositeSubscription();
+        mAddTaskView.setPresenter(this);
+    }
+
+    @Override
+    public void subscribe() {
+        if (mTaskId != null) {
+            populateTask();
+        }
+    }
+
+    @Override
+    public void unsubscribe() {
+        mSubscriptions.clear();
+    }
+
+    @Override
+    public void saveTask(String title, String description) {
+        Task newTask = mTaskId == null ?
+                new Task(title, description) :
+                new Task(title, description, mTaskId);
+        saveTask(newTask);
+    }
+
+    private void saveTask(@NonNull Task task) {
+        if (task.isEmpty()) {
+            mAddTaskView.showEmptyTaskError();
+        } else {
+            mTasksRepository.saveTask(task);
+            mAddTaskView.showTasksList();
+        }
+    }
+
+    @Override
+    public void populateTask() {
+        if (mTaskId == null) {
+            throw new RuntimeException("populateTask() was called but task is new.");
+        }
+        Subscription subscription = mTasksRepository
+                .getTask(mTaskId)
+                .subscribeOn(mSchedulerProvider.computation())
+                .observeOn(mSchedulerProvider.ui())
+                .subscribe(new Observer<Task>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        if (mAddTaskView.isActive()) {
+                            mAddTaskView.showEmptyTaskError();
+                        }
+                    }
+
+                    @Override
+                    public void onNext(Task task) {
+                        if (mAddTaskView.isActive()) {
+                            mAddTaskView.setTitle(task.getTitle());
+                            mAddTaskView.setDescription(task.getDescription());
+                        }
+                    }
+                });
+
+        mSubscriptions.add(subscription);
+    }
+}
